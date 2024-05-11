c6a02c56875800b1b01350c0ec84c3d253a67ebc
Nayem Mursalin <nayem@Nayems-MacBook-Pro-M1-7.local>
2024-05-11 14:33:11 +0600

    Section title

---
+++ yummy-kitchen-client/src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,14 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
+const PopularMenu = () => {
+  return (
+    <section>
+      <SectionTitle
+        heading="From Our Menu"
+        subHeading="Popular Item"
+      ></SectionTitle>
+    </section>
+  );
+};
+
+export default PopularMenu;
