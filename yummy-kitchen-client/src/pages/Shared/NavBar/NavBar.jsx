18aaf92c32ddf8fc728c6bc215eb0174e35be139
Nayem Mursalin <nayem@Nayems-MacBook-Pro-M1-7.local>
2024-05-17 15:56:52 +0600

    Login Page

--- yummy-kitchen-client/src/pages/Shared/NavBar/NavBar.jsx
+++ yummy-kitchen-client/src/pages/Shared/NavBar/NavBar.jsx
@@ -13,7 +13,7 @@ const NavBar = () => {
         <Link to="/order/salad">Order</Link>
       </li>
       <li>
-        <Link to="/">Our Shop</Link>
+        <Link to="/login">Login</Link>
       </li>
     </>
   );
