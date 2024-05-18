57aa8f881d3618589ed9f401c95f5445aa467ba9
Nayem Mursalin <nayem@Nayems-MacBook-Pro-M1-7.local>
2024-05-18 14:58:03 +0600

    Delete method

--- yummy-kitchen-server/index.js
+++ yummy-kitchen-server/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.piwtksr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -57,8 +57,16 @@ async function run() {
 
         app.post('/carts', async (req, res) => {
             const item = req.body;
-            console.log("Hi Nayem -> ", item);
+            // console.log("Hi Nayem -> ", item);
             const result = await cartCollection.insertOne(item);
+            res.send(result);
+        })
+
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            console.log("Hi Nayem -> ", id);
+            const result = await cartCollection.deleteOne(query);
             res.send(result);
         })
 
