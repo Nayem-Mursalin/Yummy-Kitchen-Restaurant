9b817d2011a98e7d783b1e9442315362dfb106c7
Nayem Mursalin <nayem@Nayems-MacBook-Pro-M1-7.local>
2024-05-18 14:58:40 +0600

    Dashboard Delete

--- yummy-kitchen-client/src/Layout/Dashboard.jsx
+++ yummy-kitchen-client/src/Layout/Dashboard.jsx
@@ -3,6 +3,7 @@ import {
   FaCalendar,
   FaHome,
   FaList,
+  FaSearch,
   FaShoppingCart,
 } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
@@ -43,11 +44,24 @@ const Dashboard = () => {
               My Bookings
             </NavLink>
           </li>
+          <div className="divider"></div>
+          <li>
+            <NavLink to="/">
+              <FaHome></FaHome>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/order/salad">
+              <FaSearch></FaSearch>
+              Menu
+            </NavLink>
+          </li>
         </ul>
       </div>
 
       {/* Dashboard Content  */}
-      <div className="flex-1">
+      <div className="flex-1 p-8">
         <Outlet></Outlet>
       </div>
     </div>
