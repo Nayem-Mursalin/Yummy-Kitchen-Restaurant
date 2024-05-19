c2b60bed15888d35f8c6ef2127a865647c9eb987
Nayem Mursalin <nayem@Nayems-MacBook-Pro-M1-7.local>
2024-05-19 20:07:58 +0600

    Display all user

--- yummy-kitchen-client/src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ yummy-kitchen-client/src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,15 +1,42 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FaTrash, FaUsers } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
   const axiosSecure = useAxiosSecure();
-  const { data: users = [] } = useQuery({
+  const { refetch, data: users = [] } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
       const res = await axiosSecure.get("/users");
       return res.data;
     },
   });
+
+  const handleDeleteUser = (user) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/users/${user._id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch();
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
   return (
     <div>
       <div className="flex justify-evenly my-4">
@@ -17,23 +44,39 @@ const AllUsers = () => {
         <h2 className="text-3xl">Total User: {users.lenght}</h2>
       </div>
       <div className="overflow-x-auto">
-        <table className="table table-zebra">
+        <table className="table table-zebra w-full">
           {/* head */}
           <thead>
             <tr>
               <th></th>
               <th>Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Action</th>
             </tr>
           </thead>
           <tbody>
-            {users.map((user) => (
+            {users.map((user, index) => (
               <tr key={user.id}>
-                <th>1</th>
-                <td>Cy Ganderton</td>
-                <td>Quality Control Specialist</td>
-                <td>Blue</td>
+                <th>{index + 1}</th>
+                <td>{user.displayName}</td>
+                <td>{user.email}</td>
+                <td>
+                  <button
+                    onClick={() => handleDeleteUser(user._id)}
+                    className="btn bg-orange-500"
+                  >
+                    <FaUsers className="text-white"></FaUsers>
+                  </button>
+                </td>
+                <td>
+                  <button
+                    onClick={() => handleDeleteUser(user._id)}
+                    className="btn btn-ghost"
+                  >
+                    <FaTrash className="text-red-500"></FaTrash>
+                  </button>
+                </td>
               </tr>
             ))}
             {/* row 1 */}
